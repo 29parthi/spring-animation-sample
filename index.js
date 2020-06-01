0ba47b6ca39623a2f55ae3631d4968c316db672b
29parthi <29parthiban@gmail.com>
2020-06-01 19:19:35 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
-import Container from './Container';
 import {Spring,config,Trail,Transition,animated,Keyframes} from 'react-spring/renderprops'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: true
+    };
+  }
+
+  toggle=e=>{
+    this.setState(state=> ({show: !state.show}))
+  }
+
   render() {
-    
     return (
-       
-<div>
-<Spring
- native from={{x: 100}} to={{x: 0}}
-  from={{color:'black', width:220,fontSize:100, background:
-      'linear-gradient(to right, #30e8bf, #ff8235)' }}
-  to={{color:'white', width:450,fontSize:200,   background: 'linear-gradient(to right, #fc0142, #FBD786)'}}
-  config={config.molasses}
-  reset={false}>
-  {props =>   <animated.div style={props}>Hello</animated.div> }
-</Spring>
-</div>
+      <div onClick={this.toggle}>
+        <Transition
+          native
+          items={this.state.show}
+          from={{ fontSize:100, position: 'absolute', overflow: 'hidden', height: 0 }}
+          enter={[{ height: 'auto' }]}
+          leave={{ height: 0 }}
+          config={config.molasses}>
+          {show =>
+            show ? (props => <animated.div style={props}>hello</animated.div>)
+            : (props => <animated.div style={props}>amigo</animated.div>)
+          }
+        </Transition>
+      </div>
     );
   }
 }
