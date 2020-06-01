c3e32cb9b6e35601ad76b9bd3ff3a30658364f7e
29parthi <29parthiban@gmail.com>
2020-06-01 18:42:00 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import Container from './Container';
+import {Spring,config,Trail,Transition,animated,Keyframes} from 'react-spring/renderprops'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
   render() {
+    
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+       
+<div>
+<Spring
+ native from={{x: 100}} to={{x: 0}}
+  from={{color:'black', width:220,fontSize:100, background:
+      'linear-gradient(to right, #30e8bf, #ff8235)' }}
+  to={{color:'white', width:450,fontSize:200,   background: 'linear-gradient(to right, #fc0142, #FBD786)'}}
+  config={config.molasses}
+  reset={false}>
+  {props =>   <animated.div style={props}>Hello</animated.div> }
+</Spring>
+</div>
     );
   }
 }
